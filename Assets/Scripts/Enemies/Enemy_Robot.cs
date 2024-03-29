75dbde5dca5f43aac99c70bbcf6ca358aca01c09
EMPTY280_ON_DS <1123pjs@gmail.com>
2024-03-29 16:37:40 +0900

    Added BoxMeleeAttack

--- Assets/Scripts/Enemies/Enemy_Robot.cs
+++ Assets/Scripts/Enemies/Enemy_Robot.cs
@@ -11,17 +11,20 @@ public class Enemy_Robot : Enemy
     [Header("상태 전환")]
     [SerializeField][Range(0f, 1f)] private float chargeChance = 0.5f;
     [SerializeField][Range(0f, 1f)] private float swingChance = 0.5f;
+    [SerializeField][Range(0f, 1f)] private float hitDownChance = 0.5f;
 
     EnemyStateMeleeArc melee1State;
     EnemyStateChargeAttack chargeState;
     EnemyStateAttack projectileState;
     EnemyStateMeleeArc swingState;
+    EnemyStateMeleeBox melee2State;
     protected override void OnAwake()
     {
         melee1State = (EnemyStateMeleeArc)StateList[1];
         chargeState = (EnemyStateChargeAttack)StateList[2];
         projectileState = (EnemyStateAttack)StateList[3];
         swingState = (EnemyStateMeleeArc)StateList[4];
+        melee2State = (EnemyStateMeleeBox)StateList[5];
 
         SetDefaultState(0);
     }
@@ -36,7 +39,12 @@ public class Enemy_Robot : Enemy
                     if (Random.value < swingChance && swingState.CanAttackTarget())
                         SetState(4);
                     else
-                        SetState(1);
+                    {
+                        if (Random.value < hitDownChance && melee2State.CanAttackTarget())
+                            SetState(5);
+                        else
+                            SetState(1);
+                    }
                 }
                 if (StateDuration > chaseTimeMax)
                 {
@@ -60,6 +68,10 @@ public class Enemy_Robot : Enemy
                 break;
             case 4:
                 if (swingState.IsAttackOver())
+                    SetState(0);
+                break;
+            case 5:
+                if (melee2State.IsAttackOver())
                     SetState(0);
                 break;
         }
